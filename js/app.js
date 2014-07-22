639ab88cb1d1b3bc32b20ab46fe64bc8a6122df1
Flor <florrts@gmail.com>
2014-07-22 16:56:29 +0200

    first commit

---
+++ js/app.js
@@ -0,0 +1,16 @@
+angular.module("uiRouter", ['ui.router'])
+
+.config(function($stateProvider, $urlRouterProvider) {
+  $stateProvider
+    .state('login', {
+        url: '/login',
+        templateUrl: 'login.html'})
+
+    .state('areas', {
+        url: '/areas',
+        templateUrl: 'areas.html'})
+
+    .state('reservas', {
+        url: '/reservas',
+        templateUrl: 'reservas.html'})
+})
